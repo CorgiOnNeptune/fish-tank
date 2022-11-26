2eff08ac951c98a3aad3845f416fba9f7c30966d
Justin <justinjdbennett@gmail.com>
2022-11-26 01:32:50 +0000

    Update switch-fish image

--- public/scripts/entities/bite_fish.js
+++ public/scripts/entities/bite_fish.js
@@ -3,6 +3,8 @@ class BiteFish extends Fish {
   constructor(options) {
     super(options);
     this.imageUri = '/images/bite-fish.gif';
+    this.width = 90;
+    this.height = 90;
   }
 
 }
--- public/scripts/entities/switch_fish.js
+++ public/scripts/entities/switch_fish.js
@@ -1,5 +1,11 @@
 class SwitchFish extends Fish {
 
+  constructor(options) {
+    super(options);
+    this.imageUri = '/images/switch-fish.png';
+    this.width = 90;
+  }
+
   onClick(event) {
     this.makeNewVelocity(50);
   }
